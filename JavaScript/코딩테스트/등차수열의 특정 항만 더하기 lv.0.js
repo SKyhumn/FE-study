06640f77a3b0237a9c3818b6a2aec567f51cac6a
최준영 <168086573+SKyhumn@users.noreply.github.com>
2025-01-14 23:11:54 +0900

    Delete JavaScript/코딩테스트 directory

--- JavaScript/코딩테스트/등차수열의 특정 항만 더하기 lv.0.js
+++
@@ -1,9 +0,0 @@
-function solution(a, d, included) {
-    var answer = 0;
-    for(let x=0;x<included.length;x++){
-        if(included[x]==true){
-            answer+=(a+d*x);
-        }
-    }
-    return answer;
-}
--- JavaScript/코딩테스트/문자 리스트를 문자열로 변환하기 lv.0.js
+++
@@ -1,5 +0,0 @@
-function solution(arr) {
-    var answer = '';
-    answer=arr.join('');
-    return answer;
-}
